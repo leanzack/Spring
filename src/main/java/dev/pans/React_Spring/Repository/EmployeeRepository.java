295cca48c3047fd1382feb111e09ebbe88187ef8
leyan <paninsorolean@gmail.com>
2024-08-04 19:34:33 +0800

    Revert "sample"
    This reverts commit d818975c771706d3ecf2127768b6bfc28f95cd44.

--- src/main/java/dev/pans/React_Spring/Repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.pans.React_Spring.Repository;
-
-import dev.pans.React_Spring.Entities.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface EmployeeRepository  extends JpaRepository<Employee, Long> {
-}
